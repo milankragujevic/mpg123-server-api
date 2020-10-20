357206ba4864d9696e6e44a41e3863edcd02b3c3
Milan Kragujević <milankragujevic@gmail.com>
2020-10-20 16:25:32 +0200

    Update stop.js

--- routes/stop.js
+++ routes/stop.js
@@ -1,11 +1,9 @@
-/*jslint node:true*/
 var express = require('express'),
     router = express.Router(),
     mpg123 = require('../libs/mpg123');
 
 /* /stop */
 router.get('/', function (req, res, next) {
-    'use strict';
     var data = {};
     data.httpStatus = 200;
     mpg123.stop();
